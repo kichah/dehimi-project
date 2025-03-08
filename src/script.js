a7cf6fdf371f8aa5f89a14a052c9f33e0d7c6821
tofi05 <abdellatifkichah@gmail.com>
2025-03-08 19:10:53 +0100

    navbar responsive -1

--- src/script.js
+++ src/script.js
@@ -19,3 +19,16 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  const visibility = primaryNav.getAttribute('data-visible');
+  if (visibility === 'false') {
+    primaryNav.setAttribute('data-visible', 'true');
+    navToggle.setAttribute('aria-expanded', 'true');
+  } else {
+    primaryNav.setAttribute('data-visible', 'false');
+    navToggle.setAttribute('aria-expanded', 'false');
+  }
+});
