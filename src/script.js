ea670be38fe52693b8b2c8f7edf66877d892dc81
kichah <abdellatifkichah@gmail.com>
2025-03-09 20:13:57 +0100

    finish nav

--- src/script.js
+++ src/script.js
@@ -1,12 +1,21 @@
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
+  slidesPerView: 2,
   spaceBetween: 8, // Add space between slides
   breakpoints: {
     // Responsive breakpoints
     320: {
       // when window width is >= 320px
-      slidesPerView: 4,
+      slidesPerView: 2,
+      spaceBetween: 8,
+    },
+    480: {
+      slidesPerView: 3,
       spaceBetween: 12,
+    },
+    800: {
+      // when window width is >= 320px
+      slidesPerView: 4,
+      spaceBetween: 16,
     },
     1040: {
       // when window width is >= 768px
@@ -19,16 +28,19 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
-const primaryNav = document.querySelector('.primary-navigation');
-const navToggle = document.querySelector('.mobile-nav-toggle');
-
-navToggle.addEventListener('click', () => {
-  const visibility = primaryNav.getAttribute('data-visible');
-  if (visibility === 'false') {
-    primaryNav.setAttribute('data-visible', 'true');
-    navToggle.setAttribute('aria-expanded', 'true');
-  } else {
-    primaryNav.setAttribute('data-visible', 'false');
+const primaryNav = document.querySelector('#primary-navigation');
+const navToggle = document.querySelector('.icons');
+const close = document.querySelector('.bx-x');
+const menu = document.querySelector('.bx-menu');
+console.log(navToggle);
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    primaryNav.classList.toggle('hide');
     navToggle.setAttribute('aria-expanded', 'false');
-  }
-});
+    close.classList.toggle('hidden');
+    menu.classList.toggle('hidden');
+    if (menu.classList.contains('hidden')) {
+      navToggle.setAttribute('aria-expanded', 'true');
+    }
+  });
+}
